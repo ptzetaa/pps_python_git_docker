efd9f9990da91d947cbe0ed39e394e1d0edabd4a
Eduardo Botias <edubthingshome@gmail.com>
2024-02-02 19:17:53 +0100

    fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # app.py
+
 from flask import Flask
 from bayeta import frotar
 
@@ -9,11 +10,12 @@ def index():
     # Llama a la función frotar sin limitar la cantidad de frases
     frases_auspiciosas = frotar()
 
-    # Extrae la primera frase auspiciosa
-    frase_auspiciosa = frases_auspiciosas[0]['frase']
-
-    # Devuelve una frase auspiciosa aleatoria en la respuesta de la ruta raíz
-    return frase_auspiciosa
+    if frases_auspiciosas:
+        # Extrae la primera frase auspiciosa si la lista no está vacía
+        frase_auspiciosa = frases_auspiciosas[0]['frase']
+        return f"<html><body><p>Frase Auspiciosa: {frase_auspiciosa}</p></body></html>"
+    else:
+        return "<html><body><p>No hay frases auspiciosas disponibles</p></body></html>"
 
 @app.route('/frotar/<int:n_frases>', methods=['GET'])
 def obtener_frases(n_frases):
