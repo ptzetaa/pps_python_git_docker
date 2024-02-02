f792eb6ab8dc0064e3c02307eb05050cb5d50c94
Eduardo Botias <edubthingshome@gmail.com>
2024-02-02 19:02:56 +0100

    fix

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+# app.py
+from flask import Flask
 from bayeta import frotar
 
 app = Flask(__name__)
@@ -8,13 +9,16 @@ def index():
     # Llama a la función frotar sin limitar la cantidad de frases
     frases_auspiciosas = frotar()
 
+    # Extrae la primera frase auspiciosa
+    frase_auspiciosa = frases_auspiciosas[0]['frase']
+
     # Devuelve una frase auspiciosa aleatoria en la respuesta de la ruta raíz
-    return jsonify({"frase": frases_auspiciosas[0]})
+    return frase_auspiciosa
 
 @app.route('/frotar/<int:n_frases>', methods=['GET'])
 def obtener_frases(n_frases):
     frases = frotar(n_frases)
-    return jsonify({"frases": frases})
+    return {"frases": frases}
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
