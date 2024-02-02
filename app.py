abe709c558f77b09db0ba3d7c18fba13d717ec5b
Eduardo Botias <edubthingshome@gmail.com>
2024-02-02 17:04:18 +0100

    Agregadas rutas de Flask y modificada la función frotar

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Hola, mundo")
+from flask import Flask, jsonify
+from bayeta import frotar
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hola mundo'
+
+@app.route('/frotar/<int:n_frases>', methods=['GET'])
+def frotar_endpoint(n_frases):
+    frases = frotar(n_frases)
+    return jsonify({"frases": frases})
+
+if __name__ == '__main__':
+    app.run(debug=True)
