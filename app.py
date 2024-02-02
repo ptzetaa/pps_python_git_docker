61170685fb1e1939313b2623af87cbda593db1fc
Eduardo Botias <edubthingshome@gmail.com>
2024-02-02 17:57:06 +0100

    fix

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 from bayeta import frotar
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    frases = frotar(1)
-    return jsonify({"mensaje": "Hola mundo", "frases": frases})
+    frase = frotar(1)[0]
+    return render_template('index.html', frase=frase)
 
 @app.route('/frotar/<int:n_frases>', methods=['GET'])
 def frotar_endpoint(n_frases):
-    frases = frotar(n_frases)
-    return "\n".join(frases)
+    if n_frases == 1:
+        frases = frotar(n_frases)
+        return jsonify({"frases": frases})
+    else:
+        frase = frotar(1)[0]
+        return render_template('index.html', frase=frase)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
