9c475b9d7edd288302e17f7b1502b7ac254fab39
Eduardo Botias <edubthingshome@gmail.com>
2024-02-03 15:58:24 +0100

    ultima version

--- bayeta.py
+++ bayeta.py
@@ -1,8 +1,12 @@
 # bayeta.py
-from moodle import instanciar, consultar
+from moodle import instanciacion, consulta, insercion
 
-def frotar(n_frases: int = 1):
-    cliente = instanciar()
-    frases = consultar(cliente, n_frases)
+def frotar(n_frases: int = 1) -> list:
+    cliente, frases_auspiciosas = instanciacion()
+    resultado = consulta(cliente, frases_auspiciosas, n_frases)
+    return resultado
+
+def agregar_frase_auspiciosa(nueva_frase):
+    cliente, frases_auspiciosas = instanciacion()
+    insercion(frases_auspiciosas, nueva_frase)
     cliente.close()
-    return frases
